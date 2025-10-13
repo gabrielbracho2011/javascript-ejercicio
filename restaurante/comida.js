dacac12487df983e7a13a1b38378c22a0e630050
ZORO🤎 <shabigabrielsan5@gmail.com>
2025-10-12 21:03:42 -0300

    primer commit
    ejercicio javascript

---
+++ restaurante/comida.js
@@ -0,0 +1,14 @@
+var comida1 = 2800;
+var comida2 = 1300;
+var bebida1 = 900;
+var bebida2 = 650;
+
+var total_comida = comida1 + comida2;
+var total_bebida = bebida1 + bebida2;
+var total_comida_bebida = total_comida + total_bebida;
+
+console.log("El total de comida es: " + total_comida);
+console.log("El total de bebida es: " + total_bebida);
+console.log("El total de comida mas bebida es: " + total_comida_bebida);
+
+alert("El total es: " + total_comida_bebida);
